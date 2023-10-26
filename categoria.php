5541cc5d3df47d6f6fce36e4cbc6f2cf3ff00703
Nikerson Patrick <79768540+Nikerson-P@users.noreply.github.com>
2023-10-26 12:34:55 -0300

    Update categoria.php

--- categoria.php
+++ categoria.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 
-<form action="categoria_cad.php" method="">
+<form action="categoria_cad.php" method="POST">
   <input type="text" length="20" name="categoria" placeholder="Digite o nome da categoria">
   <button type="submit">Cadastrar</button>
   
