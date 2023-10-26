fde6b038c0f6e18f481b3624db636e4ffa5903b4
Nikerson Patrick <79768540+Nikerson-P@users.noreply.github.com>
2023-10-26 12:33:08 -0300

    Create categoria.php

---
+++ categoria.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Criar categorias</title>
+</head>
+<body>
+
+<form action="categoria_cad.php" method="">
+  <input type="text" length="20" name="categoria" placeholder="Digite o nome da categoria">
+  <button type="submit">Cadastrar</button>
+  
+</form>
+</body>
+</html>
